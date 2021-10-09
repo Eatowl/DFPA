911ed3c36148bb509f296471769acb6528c34086
Eatowl <csnelcov@gmail.com>
2021-10-09 19:12:13 +0300

    Remove tkinter code

--- interface.py
+++ interface.py
@@ -18,46 +18,6 @@ class Interface():
 		self.test = [1,2,3]
 
 
-	def getResolution(self, window):
-		window.update_idletasks()
-		screen_width = window.winfo_screenwidth()
-		screen_height = window.winfo_screenheight()
-
-		# определение разрешения при помощи сторонних источников линукса и винды
-
-		#t.update()
-		window.attributes('-zoomed', True)
-		#window.state('zoomed')
-		m_1_height= window.winfo_height()
-		m_1_width= window.winfo_width()
-
-		print(m_1_width, m_1_height)
-		print("*"*80)
-		print(screen_width, screen_height)
-
-		screen_resolution = str(screen_width)+'x'+str(screen_height)
-
-		return screen_resolution
-
-
-	def checkGeometry(self):
-		if platform == "linux" or platform == "linux2":
-    		# linux
-			print("LINUX")
-		elif platform == "darwin":
-    		# OS X
-			print("OSX")
-		elif platform == "win32":
-			print("WINDOWS")
-		
-
-	def createInterface(self):
-		window = Tk()
-		#screen_resolution = self.getResolution(window)
-		window.title("DFPA V-0.02")
-		window.geometry('640x420')
-		return window
-
 	def startProcess(self):
 		if self.data is not None:
 			clear_data = UserControl(self.data)
@@ -66,17 +26,6 @@ class Interface():
 		else:
 			print("Data not find!")
 		print("END")
-
-
-	def clicked(self, txt, lbl):
-		self.data = txt.get()
-		lbl.configure(text=self.data)
-
-
-	def printDataCheck(self, window):
-		lbl = Label(window, text="Данные для анализа: {}".format(self.data),
-							font=("Arial Bold", 10))  
-		lbl.grid(column=0, row=0)
 
 
 	def printInterface(self):
