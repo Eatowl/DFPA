bc5d5726538dba3a09f65b09e13413207b4a8598
Eatowl <csnelcov@gmail.com>
2021-10-11 18:28:35 +0300

    Added function 'searchForPasses' and call this function

--- problemSearch.py
+++ problemSearch.py
@@ -21,7 +21,31 @@ class ProblemSearch():
 		self.roadMap = roadMap
 		self.test = [1,2,3]
 
+	def searchForPasses(self):
+		logging.info("Start ProblemSearch 'searchForPasses' function")
+		logging.debug("*"*80)
+		logging.debug(self.df.isna().sum())
+		logging.debug(self.df.columns.values)
+		logging.debug("*"*80)
+		self.roadMap.check_of_passes = True
+		for valueAndName in zip(self.df.isna().sum(), self.df.columns.values):
+			if valueAndName[0] > 0:
+				self.roadMap.check_of_passes_data.append(valueAndName[1])
+
 	def startSearch(self):
 		logging.info("Start ProblemSearch 'startSearch' function")
-		self.roadMap = ['Q', 'W', 'E']
+		logging.debug("ProblemSearch 'startSearch' function."\
+						" Value check_of_passes: {}"\
+								.format(self.roadMap.check_of_passes))
+		if self.roadMap.check_of_passes is False:
+			self.searchForPasses()
+		logging.debug("#################################")
+		logging.debug("ProblemSearch 'startSearch' function."\
+						" Value check_of_passes_data: {}"\
+								.format(self.roadMap.check_of_passes_data))
+		logging.debug("ProblemSearch 'startSearch' function."\
+						" Value check_of_passes: {}"\
+								.format(self.roadMap.check_of_passes))
+		logging.debug("#################################")
+		
 		return [1,2,3], self.roadMap
