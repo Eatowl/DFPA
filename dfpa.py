116bc6828e87eeacf623bd81b2739df912fa3917
Eatowl <csnelcov@gmail.com>
2021-10-11 18:26:50 +0300

    Remove spaces

--- dfpa.py
+++ dfpa.py
@@ -39,25 +39,21 @@ def main(argv = None):
 
     if argv is None:
         argv = sys.argv
-
     try:
         try:
             opts, args = getopt.getopt(argv[1:], "h", ["help"])
-
             if len(args) != 0:
                 logger.debug('dfpa main run userControl data: {}'.format(args[0]))
                 status_userControl = startUserControl(args[0])
             else:
                 logger.debug('dfpa main run userControl data: Empty')
                 status_userControl = startUserControl(None)
-
         except getopt.error as msg:
              raise Usage(msg)
-
     except Usage as err:
         print >> sys.stderr, err.msg
         print >> sys.stderr, "for help use --help"
-
+        
         return 2
 
 if __name__ == "__main__":
